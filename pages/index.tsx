f3d0d5d0522ab6f80864b1b62fcf13169c6f8d5f
vharkins <105030530+vharkins1@users.noreply.github.com>
2023-12-11 19:57:19 -0800

    adding burrito Zach and JC's dunk

--- pages/index.tsx
+++ pages/index.tsx
@@ -135,6 +135,18 @@ const HomePage: React.FC = () => {
         width={320}  // Set the width of the image
         height={410} // Set the height of the image
       /> 
+      <Image
+        src="/images/burritoZach.jpeg"  // Adjust the path based on your folder structure
+        alt="My Image"
+        width={320}  // Set the width of the image
+        height={410} // Set the height of the image
+      /> 
+      <Image
+        src="/images/JCdunk.jpeg"  // Adjust the path based on your folder structure
+        alt="My Image"
+        width={320}  // Set the width of the image
+        height={410} // Set the height of the image
+      /> 
       {/* Add a video using the HTML5 video element */}
       <video width={320} height={410} controls>
        <source src="video/video.mp4" type="video/mp4"/>
